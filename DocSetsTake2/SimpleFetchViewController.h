9d19a0428dd39d4de71be6bde3eb47f647efb2c3
Andy Lee <aglee@mac.com>
2016-04-18 18:28:13 -0400

    New view controller that will help with testing.

---
+++ DocSetsTake2/SimpleFetchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SimpleFetchViewController.h
+//  DocSetsTake2
+//
+//  Created by Andy Lee on 4/18/16.
+//  Copyright © 2016 Andy Lee. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SimpleFetchViewController : NSViewController
+
+@end
---
+++ DocSetsTake2/SimpleFetchViewController.m
@@ -0,0 +1,22 @@
+//
+//  SimpleFetchViewController.m
+//  DocSetsTake2
+//
+//  Created by Andy Lee on 4/18/16.
+//  Copyright © 2016 Andy Lee. All rights reserved.
+//
+
+#import "SimpleFetchViewController.h"
+
+@interface SimpleFetchViewController ()
+
+@end
+
+@implementation SimpleFetchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
