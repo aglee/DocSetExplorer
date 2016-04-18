e948d0f0307ec857383e64db800295ab3d80e98f
Andy Lee <aglee@mac.com>
2016-04-17 23:26:15 -0400

    Added QuietLog to the project.

---
+++ DocSetsTake2/QuietLog.h
@@ -0,0 +1,21 @@
+//
+//  QuietLog.h
+//  AppKiDo
+//
+//  Created by Andy Lee on 4/4/13.
+//  Copyright (c) 2013 Andy Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/*!
+ * Credit: Mark Dalrymple <http://borkware.com/quickies/single?id=261>.
+ */
+extern void QuietLog (NSString *format, ...);
+
+
+#define REPLACE_NSLOG_WITH_QUIETLOG 1
+#if REPLACE_NSLOG_WITH_QUIETLOG
+#define NSLog QuietLog
+#endif
+
