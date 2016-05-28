b63973e5f48fcce4fcffb2f839ebacab723af28d
Andy Lee <aglee@mac.com>
2016-05-27 20:34:56 -0400

    Changed the text views in the advanced-search pane so they run the query when you hit Return, instead of inserting a newline.

---
+++ DocSetExplorer/Utilities/DSETextView.h
@@ -0,0 +1,15 @@
+//
+//  DSETextView.h
+//  DocSetExplorer
+//
+//  Created by Andy Lee on 5/27/16.
+//  Copyright © 2016 Andy Lee. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+/*!
+ * Sends an action to first responder when you hit Return.
+ */
+@interface DSETextView : NSTextView
+@end
---
+++ DocSetExplorer/Utilities/DSETextView.m
@@ -0,0 +1,19 @@
+//
+//  DSETextView.m
+//  DocSetExplorer
+//
+//  Created by Andy Lee on 5/27/16.
+//  Copyright © 2016 Andy Lee. All rights reserved.
+//
+
+#import "DSETextView.h"
+#import "DocSetExplorerWindowController.h"
+
+@implementation DSETextView
+
+- (void)insertNewline:(id)sender
+{
+	(void)[NSApp sendAction:@selector(doSearch:) to:nil from:nil];
+}
+
+@end
